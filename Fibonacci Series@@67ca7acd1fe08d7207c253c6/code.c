85be4d1a052693fe431861f084e2dd3a935b2304
Vanshika Mahajan <mm8493348@gmail.com>
2025-03-07 11:42:20 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,7 +1,8 @@
 // Your code here...
+#include<stdio.h>
 int fibonacciSeries(int n){
     if(n==0){
-        return n;
+        return 0;
     }
     else{
         return fibonacciSeries(n-1)+fibonacciSeries(n-2);
