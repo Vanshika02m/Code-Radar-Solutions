376f5a17365e13a4ebe07488436ab4d73c68b270
Vanshika Mahajan <mm8493348@gmail.com>
2025-03-30 00:24:21 +0530

    Solution to Print a Pyramid Pattern

--- Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
+++ Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
@@ -1,49 +1,15 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main(){
-    int n;
+    int nnst=1;
     scanf("%d",&n);
-    if(n==1){
-        printf("*\n");
-    }
-    if(n==2){
-        printf(" * \n");
-        printf("***\n");
-    }
-    if(n==3){
-        printf("  * \n");
-        printf(" *** \n");
-        printf("*****\n");
-    }
-    if(n==4){
-        printf("   *   \n");
-        printf("  ***  \n");
-        printf(" ***** \n");
-        printf("*******\n");
-    }
-    if(n==5){
-        printf("    *    \n");
-        printf("   ***   \n");
-        printf("  *****  \n");
-        printf(" ******* \n");
-        printf("*********\n");
-    }
-    if(n==6){
-        printf("     *     \n");
-        printf("    ***    \n");
-        printf("   *****   \n");
-        printf("  *******  \n");
-        printf(" ********* \n");
-        printf("***********\n");
-    }
-    if(n==7){
-        printf("      *      \n");
-        printf("     ***     \n");
-        printf("    *****    \n");
-        printf("   *******   \n");
-        printf("  *********  \n");
-        printf(" *********** \n");
-        printf("*************\n");
-
+    for(int i=1;i<=n;i++){
+        for(int k=1;k<=n-i;k++){
+            printf(" ");
+        }
+        for(int j=1;j<=nst;j++){
+            printf("%d ",j);
+        }
+        nst+=2;
+        printf("\n");
     }
 }
