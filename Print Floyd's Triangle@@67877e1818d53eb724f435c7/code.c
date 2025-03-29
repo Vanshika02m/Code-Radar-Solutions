191bc787cdf3d85ddb4238f24eb27d3ddc45f86c
Vanshika Mahajan <mm8493348@gmail.com>
2025-03-30 00:18:30 +0530

    Solution to Print Floyd's Triangle

--- Print Floyd's Triangle@@67877e1818d53eb724f435c7/code.c
+++ Print Floyd's Triangle@@67877e1818d53eb724f435c7/code.c
@@ -1,49 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 int main(){
     int n;
+    int num=1;
     scanf("%d",&n);
-    if(n==1){
-        printf("1\n");
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            num++;
+        }
+        printf("%d ",num);
     }
-    if(n==2){
-        printf("1\n");
-        printf("2 3\n");
-    }
-    if(n==3){
-        printf("1\n");
-        printf("2 3\n");
-        printf("4 5 6\n");
-    }
-    if(n==4){
-        printf("1\n");
-        printf("2 3 \n");
-        printf("4 5 6\n");
-        printf("7 8 9 10\n");
-    }
-    if(n==5){
-        printf("1\n");
-        printf("2 3 \n");
-        printf("4 5 6 \n");
-        printf("7 8 9 10\n");
-        printf("11 12 13 14 15");
-    }
-    if(n==6){
-        printf("1\n");
-        printf("2 3 \n");
-        printf("4 5 6 \n");
-        printf("7 8 9 10\n");
-        printf("11 12 13 14 15\n");
-        printf("16 17 18 19 20 21\n");
-    }
-    if(n==7){
-        printf("1\n");
-        printf("2 3 \n");
-        printf("4 5 6\n");
-        printf("7 8 9 10\n");
-        printf("11 12 13 14 15\n");
-        printf("16 17 18 19 20 21\n");
-        printf("22 23 24 25 26 27 28");
-    }
-
-            
 }
