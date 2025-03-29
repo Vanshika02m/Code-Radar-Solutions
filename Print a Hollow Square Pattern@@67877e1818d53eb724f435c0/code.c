37cd5e40e5841834ac3155ad0f296730c75840fa
Vanshika Mahajan <mm8493348@gmail.com>
2025-03-30 00:39:06 +0530

    Solution to Print a Hollow Square Pattern

--- Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
+++ Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
@@ -1,57 +1,22 @@
 #include <stdio.h>
-int main(){
+
+int main() {
     int n;
-    scanf("%d",&n);
-    if(n==2){
-        printf("**\n");
-        printf("**");
+    
+    printf("Enter the size of the square: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            // Print '*' for first and last row, or first and last column
+            if (i == 1 || i == n || j == 1 || j == n) {
+                printf("* ");
+            } else {
+                printf("  "); // Print space inside the square
+            }
+        }
+        printf("\n");
     }
-    if(n==3){
-        printf("***\n");
-        printf("* *\n");
-        printf("***\n");
-    }
-    if(n==4){
-        printf("****\n");
-        printf("*  *\n");
-        printf("*  *\n");
-        printf("****\n");
-    }
-    if(n==5){
-        printf("*****\n");
-        printf("*   *\n");
-        printf("*   *\n");
-        printf("*   *\n");
-        printf("*****\n");
-    }
-    if(n==6){
-        printf("******\n");
-        printf("*    *\n");
-        printf("*    *\n");
-        printf("*    *\n");
-        printf("*    *\n");
-        printf("*    *\n");
-        printf("******\n");
-    }
-    if(n==7){
-        printf("*******\n");
-        printf("*     *\n");
-        printf("*     *\n");
-        printf("*     *\n");
-        printf("*     *\n");
-        printf("*     *\n");
-        printf("*     *\n");
-        printf("*******\n");
-    }
-    if(n==8){
-        printf("********\n");
-        printf("*      *\n");
-        printf("*      *\n");
-        printf("*      *\n");
-        printf("*      *\n");
-        printf("*      *\n");
-        printf("*      *\n");
-        printf("*      *\n");
-        printf("********\n");
-    }
+
+    return 0;
 }
