688538fa30e8afaa1035e7f9058c00a67c425625
Vanshika Mahajan <mm8493348@gmail.com>
2025-04-09 00:27:41 +0530

    Solution to Count Elements Greater Than a Given Value

--- Count Elements Greater Than a Given Value@@676c02002dbc8c3e7a280b8a/code.c
+++ Count Elements Greater Than a Given Value@@676c02002dbc8c3e7a280b8a/code.c
@@ -1,28 +1,35 @@
-// Your code here...
-#include<stdio.h>
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-    int k;
-    scanf("%d",&k);
-    int max=arr[0];
-    for(int i=0;i<n;i++){
-        if(arr[i]%2==0){
-            if(max<arr[i]){
-                max=arr[i];
-            }
-        }
-    }
-    int count=0;
-    for(int i=0;i<n;i++){
-        if(arr[i]==max){
-            count++;
+#include <stdio.h>
+#include <limits.h>
 
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int k;  
+    scanf("%d", &k);
+
+    int max = INT_MIN;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] % 2 == 0 && arr[i] > max) {
+            max = arr[i];
         }
     }
-    printf("%d",count);
+    if(max == INT_MIN) {
+        printf("0\n");  
+        return 0;
+    }
+    int count = 0;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == max) {
+            count++;
+        }
+    }
+
+    printf("%d\n", count);
+    return 0;
 }
